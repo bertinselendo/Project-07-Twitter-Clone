72f4f818a1648da020a8ec38128def2e40428335
Bertin Selendo <contact@bertinselendo.com>
2023-12-09 17:41:14 +0100

    Set profile header back button to navigate to previous page

--- src/templates/profile/profileHeader.jsx
+++ src/templates/profile/profileHeader.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import BackIcon from "../icons/backIcon";
 import UserServices from "../../models/userServices";
+import { useNavigate } from "react-router-dom";
 
 export default function ProfileHeader() {
 
   const user = UserServices;
   const userPostsNumber = UserServices.getUserPostsNumber();
+  const navigate = useNavigate();
 
   return (
     <>
       <div className="profile-header">
         <div className="back-button">
-          <a href="#">
+          <a href="#" onClick={() => navigate(-1)}>
             <BackIcon />
           </a>
         </div>
